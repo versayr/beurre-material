863dfafb830c57e10cce11d67c7fc7f2cdbd43f1
versayr <themoose3@gmail.com>
2018-07-24 22:57:37 -0400

    Clean up cruft

--- src/App.js
+++ src/App.js
@@ -1,28 +1,13 @@
 import React          from 'react';
-import PropTypes      from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
 import Paper          from '@material-ui/core/Paper';
 import ListSubheader  from '@material-ui/core/ListSubheader';
 import List           from '@material-ui/core/List';
 import PastryCategory from './components/PastryCategory';
 
-const styles = theme => ({
-  root: {
-    width: '100%',
-    maxWidth: 360,
-    backgroundColor: theme.palette.background.paper,
-  },
-  nested: {
-    paddingLeft: theme.spacing.unit * 4,
-  },
-});
-
-class NestedList extends React.Component {
+class App extends React.Component {
   render() {
-    const { classes } = this.props;
-
     return (
-      <div className={classes.root}>
+      <div>
         <Paper>
           <List
             component="nav"
@@ -39,8 +24,4 @@ class NestedList extends React.Component {
   }
 }
 
-NestedList.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
-
-export default withStyles(styles)(NestedList);
+export default App;
